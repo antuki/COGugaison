40701e84606e377e7918dcd3ae2bc03ace55b989
antuki <kim.demarty@laposte.net>
2017-04-01 16:14:46 +0200

    01/04

--- R/agregation_nivsupra.R
+++ R/agregation_nivsupra.R
@@ -4,18 +4,20 @@
 #' @return Remplir
 #' @export
 
-agregation_nivsupra <- function(table_entree,code_com=colnames(table_entree)[1],var_num=colnames(table_entree[ ,sapply(table_entree, is.numeric) ]),nivsupra){
+agregation_nivsupra <- function(table_entree,code_com=colnames(table_entree)[1],var_num=colnames(table_entree[ ,sapply(table_entree, is.numeric) ]),nivsupra,na.nivgeo.rm=F){
 
   table_entree <- merge(table_entree,table_supracom[,c("CODGEO",nivsupra)],by.x=code_com,by.y="CODGEO",all.x=T,all.y=F)
-  table_entree <- aggregate(table_entree[,c(var_num)],by=list(table_entree[,nivsupra]),FUN="sum")
-  colnames(table_entree)[1] <- nivsupra
-  table_entree <- merge(table_entree,libelles_supracom[which(libelles_supracom$NIVGEO==nivsupra),c(2,3)],by.x=nivsupra,by.y="CODGEO",all.x=T,all.y=F)
-  table_entree <- table_entree[,c(nivsupra,"LIBGEO",var_num),]
-  return(table_entree)
+
+  table_sortie <- aggregate(table_entree[,c(var_num)],by=list(table_entree[,nivsupra]),FUN=sum)
+  colnames(table_sortie)[1] <- nivsupra
+  table_sortie <- merge(table_sortie,libelles_supracom[which(libelles_supracom$NIVGEO==nivsupra),c(2,3)],by.x=nivsupra,by.y="CODGEO",all.x=T,all.y=F)
+  table_sortie <- table_sortie[,c(nivsupra,"LIBGEO",var_num),]
+
+  if(na.nivgeo.rm==F & sum(colSums(table_entree[is.na(table_entree[,nivsupra]),var_num]))!=0 ){
+    vect_NA <- c(NA,NA,colSums(table_entree[is.na(table_entree[,nivsupra]),var_num]))
+    names(vect_NA)[1:2] <- c(nivsupra,"LIBGEO")
+    table_sortie<- rbind.data.frame(table_sortie,vect_NA)
+  }
+
+  return(table_sortie)
 }
-
-# tab <- COG2016
-# tab$col2 <- tab$POP*2
-# test<-  agregation_nivsupra(table_entree=tab,nivsupra="ZE2010")
-
-
