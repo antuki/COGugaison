97281c22314386b2e3a8c6576d414fa799a34482
stephLH <github@le-henanff.fr>
2020-03-27 11:20:58 +0100

    oubli de la doc pour le @param code

--- R/libelle.R
+++ R/libelle.R
@@ -2,7 +2,7 @@
 #' @name libelle
 #' @description Renvoie le libellé d'une entité géographique à partir de son code. Il est nécessaire de préciser le niveau géographique du code : commune, département, région, etc...
 #' @param niveau Niveau de code géographie: `COG` pour la commune et les valeurs définie dans la table `libelles_supracom` pour les niveau supra.
-#' @param code .
+#' @param code Code de l'entité géographique.
 #' @details
 #' Le code est vectorisé, il est donc possible de l'utiliser dans un data.frame pour créer une nouvelle colonne.\cr
 #' Les données de 2020, les plus récentes, sont utilisées.
